b2afb3fb1d079a5d43cec3b3f27d286e629c853c
ejrollo <ejrollo@gmail.com>
2021-09-13 22:05:07 -0700

    fixed issues with @param in repositories

--- cst438_gradebook/src/main/java/com/cst438/domain/EnrollmentRepository.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/EnrollmentRepository.java
@@ -1,6 +1,7 @@
 package com.cst438.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface EnrollmentRepository extends CrudRepository <Enrollment, Integer> {
 
