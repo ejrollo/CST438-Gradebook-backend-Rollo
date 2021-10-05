a0e3c8fe445a34f3e00e4e24da343367c298d968
EJ <ejrollo@gmail.com>
2021-10-04 18:20:59 -0700

    gg

--- cst438_gradebook/src/test/java/com/cst438/EndToEndTestAddAssignmemt.java
+++ cst438_gradebook/src/test/java/com/cst438/EndToEndTestAddAssignmemt.java
@@ -107,7 +107,7 @@ public class EndToEndTestAddAssignmemt {
 
 		try {			
 			// Locate and click Add Assignment button
-			WebElement we = driver.findElement(By.xpath("//button[span='Add']"));
+			WebElement we = driver.findElement(By.xpath("div//button[span='Add']"));
 			we.click();
 			Thread.sleep(SLEEP_DURATION);
 			
